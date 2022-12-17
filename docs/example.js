86bb87b9400531d56060a5f921e0032597a5ace9
ZhengLinLei <zheng9112003@gmail.com>
2022-12-17 18:17:24 +0100

    @v1.0

---
+++ docs/example.js
@@ -0,0 +1,27 @@
+// const json2csv = require('json2csv');
+const json2csv = require('../index');
+
+// Create a JSON array
+const json = [
+    {
+        name: "John",
+        age: 30,
+        city: "New York"
+    },
+    {
+        name: "Peter",
+        age: 40,
+        city: "Los Angeles"
+    },
+    {
+        name: "Amy",
+        age: 50,
+        city: "Chicago"
+    }
+];
+
+// Convert JSON array to CSV string
+const csv = json2csv(json);
+
+// Print CSV string
+console.log(csv);
